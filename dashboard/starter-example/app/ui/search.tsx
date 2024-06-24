d502892149bd384c253bfb88d7b27aa980743d5d
Ivan Martínez <calmartivan@gmail.com>
2024-06-24 22:53:14 +0200

    handleSearch and Table subcomponent of invoices

--- dashboard/starter-example/app/ui/search.tsx
+++ dashboard/starter-example/app/ui/search.tsx
@@ -10,7 +10,7 @@ export default function Search({ placeholder }: { placeholder: string }) {
   const pathname = usePathname();
   const { replace } = useRouter();
 
-  const handleSearch = (term: string) => {
+  function handleSearch(term: string) {
     const params = new URLSearchParams(searchParams);
     if (term){
       params.set('query', term);
@@ -31,6 +31,7 @@ export default function Search({ placeholder }: { placeholder: string }) {
         className="peer block w-full rounded-md border border-gray-200 py-[9px] pl-10 text-sm outline-2 placeholder:text-gray-500"
         placeholder={placeholder}
         onChange={(ev) => { handleSearch(ev.target.value) }}
+        defaultValue={searchParams.get('query')?.toString()}
       />
       <MagnifyingGlassIcon className="absolute left-3 top-1/2 h-[18px] w-[18px] -translate-y-1/2 text-gray-500 peer-focus:text-gray-900" />
     </div>
